bc1d363f02dd57df5bc91567e64f9b1011ca1a6f
desss2 <desiree.pizzolante@studenti.unisalento.it>
2025-06-20 19:46:45 +0200

    svotato repository

--- src/main/java/it/unisalento/iot2425/watchapp/user/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package it.unisalento.iot2425.watchapp.user.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code= HttpStatus.NOT_FOUND)
-public class UserNotFoundException extends RuntimeException {
-}
--- src/main/java/it/unisalento/iot2425/watchapp/user/security/SecurityConstants.java
+++
@@ -1,8 +0,0 @@
-package it.unisalento.iot2425.watchapp.user.security;
-
-public class SecurityConstants {
-
-    public final static String JWT_SECRET = "3267ef23fg2fg2gfisdd23g23g23g8ydg23dgy32dgy23dguy23g6qyqy7";
-    public final static String JWT_ISSUER="dfsjdsdhgasjhdbszhmdgsjhdbsdzmndsghjs";
-
-}
